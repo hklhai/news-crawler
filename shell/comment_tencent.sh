aeee55c58a671c7d3684955d3972aa69269abfcb
hklhai <hkhai@outlook.com>
2018-08-17 10:33:43 +0800

    脚本修改

--- shell/comment_tencent.sh
+++ shell/comment_tencent.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
 export PATH=$PATH:/usr/local/bin:/home/ubuntu1/.virtualenvs/news-crawler-DDLWWNG6/bin
 
-
 #进入.py脚本所在目录
-cd /home/ubuntu1/Project/news-crawler/tencentComment
+cd /home/ubuntu1/Project/news-crawler/
+
+git -c core.quotepath=false -c log.showSignature=false pull --progress --no-stat -v --progress origin master
+
+cd tencentComment
 
 #执行.py中定义的项目，并指定日志文件，其中nohup....&表示可以在后台执行，不会因为关闭终端而导致程序执行中断。
 v_date=$(date +%Y%m%d)
--- shell/tencent.sh
+++ shell/tencent.sh
@@ -2,7 +2,11 @@
 export PATH=$PATH:/usr/local/bin:/home/ubuntu1/.virtualenvs/news-crawler-DDLWWNG6/bin
 
 #进入.py脚本所在
-cd /home/ubuntu1/Project/news-crawler/tencent
+cd /home/ubuntu1/Project/news-crawler/
+
+git -c core.quotepath=false -c log.showSignature=false pull --progress --no-stat -v --progress origin master
+
+cd tencent
 
 #执行.py中定义的项目，并指定日志文件，其中nohup....&表示可以在后台执行，不会因为关闭终端而导致程序执行中断。
 v_date=$(date +%Y%m%d)
