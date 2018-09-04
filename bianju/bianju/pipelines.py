214c2b8cc0ea83f626a9412a8535e0c736be758f
hklhai <hkhai@outlook.com>
2018-09-04 14:34:22 +0800

    修正不必要的文件路径获取

--- bianju/bianju/pipelines.py
+++ bianju/bianju/pipelines.py
@@ -4,12 +4,11 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import codecs
 import json
 
 from elasticsearch import Elasticsearch
 
-from bianju.common import HOST_PORT, get_comment_file_system_path, get_now_date, SCRIPT_INDEX, SCRIPT_TYPE
+from bianju.common import HOST_PORT, SCRIPT_INDEX, SCRIPT_TYPE
 
 
 class BianjuPipeline(object):
@@ -23,7 +22,7 @@ class JsonWithEncodingPipeline(object):
     """
 
     def __init__(self):
-        self.file = codecs.open(get_comment_file_system_path() + get_now_date(), 'w', encoding="utf-8")
+        # self.file = codecs.open(get_comment_file_system_path() + get_now_date(), 'w', encoding="utf-8")
         self.es = Elasticsearch([HOST_PORT])
 
     def process_item(self, item, spider):
